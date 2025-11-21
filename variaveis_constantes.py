c4947d6649032214f22f9814b4672e95aa960668
castilhoserafim <wesleycserafim@gmail.com>
2025-11-21 17:34:02 -0300

    uptade with brazilian teams

--- variaveis_constantes.py
+++ variaveis_constantes.py
@@ -7,10 +7,10 @@ print(nome, idade)
 
 limite_saque_diario = 1000
 
-BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS", "MG"]
 
 print(BRAZILIAN_STATES)
 
-TIMES_BRASILEIROS = "São Paulo, Flamengo, Avaí, Grêmio"
+TIMES_BRASILEIROS = "São Paulo, Flamengo, Avaí, Grêmio e Cruzeiro"
 
 print(TIMES_BRASILEIROS)

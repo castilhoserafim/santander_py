109a45042111fb3a502302236f89278887925bd5
castilhoserafim <wesleycserafim@gmail.com>
2026-01-08 11:40:07 -0300

    Create string_4.py

---
+++ string_4.py
@@ -0,0 +1,26 @@
+nome = "Castilho"
+
+mensagem = f"""
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+      Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+
+print(
+    """
+    ========== OPÇÕES ==========
+
+    1 - Depositar
+    2 - Sacar
+    3 - Verfificar Saldo
+    4 - Dúvidas
+    5 - Sair
+
+    ============================
+
+            Obrigado por utilizar nosso aplicativo!  
+"""
+)
